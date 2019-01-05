2e0a4e019c37bbdaed5a98bd278d6d179b4269c1
A23611 <41458791+A23611@users.noreply.github.com>
2019-01-05 14:01:55 +0700

    a

---
+++ Day2/xoanOc.js
@@ -0,0 +1,36 @@
+function makeArray(n) {
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        array = [i];
+        arr.push(array);
+    }
+    return arr;
+}
+
+function xoanOc(n) {
+    let arr = makeArray(n);
+    let num = 1;
+    for (let i = 0; i < n/2; i++) {
+        for (let j = i; j < n - i; j++) {
+            arr[i][j] = num;
+            num ++;
+        };
+        num --;
+        for (let j = i; j < n - i; j++) {
+            arr[j][n - 1 - i] = num;
+            num ++;
+        };
+        num --;
+        for (let j = i; j < n - i; j++) {
+            arr[n - 1 -i][n - 1 - j] = num;
+            num ++;
+        };
+        num --;
+        for (let j = i; j < n - i - 1; j++) {
+            arr[n - 1 - j][i] = num;
+            num ++;
+        };
+        num --;
+    }
+    return arr;
+}
