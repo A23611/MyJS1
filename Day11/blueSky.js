58feb0dd4d024f6ed569a5e44fbe095ae7db00a2
A23611 <41458791+A23611@users.noreply.github.com>
2019-01-29 11:06:25 +0700

    up

---
+++ Day11/blueSky.js
@@ -0,0 +1,24 @@
+// de bai: dao nguoc string "the sky is blue" => "blue is sky the"
+//
+// y tuong: chay vong for nguoc string den khi gap " " thi slice,trim str va
+// rs += (" " + strSlice)
+// cuoi cung dc rs co 1 khoang trang o dau dung rs = rs.subStr(1);
+
+
+function blueSky(str) {
+    let rs = '';
+    let temp = str.length;
+    for(let i = str.length - 1; i >= 0; i --) {
+        if(str[i] == " ") {
+            rs += (" " + str.slice(i, temp).trim());
+            temp = i;
+        }
+        if(i == 0) {
+            rs += (" " + str.slice(i, temp).trim());
+        }
+    }
+    rs = rs.trim();
+    return rs;
+}
+
+console.log(blueSky("     the sky is blue        "));
